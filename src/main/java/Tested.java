577afc2ad8ad4726c2b4e6b233afdd538c2f6b9f
Alexander Shvets <box0259@gmail.com>
2022-01-13 21:45:14 +0400

    Tested serial lib

--- src/main/java/Tested.java
+++ src/main/java/Tested.java
@@ -1,2 +1,14 @@
+
+import com.fazecast.jSerialComm.SerialPort;
+
 public class Tested {
+    public static void main(String[] args) {
+        for(SerialPort element : SerialPort.getCommPorts()){
+            System.out.println(element.getSystemPortName());
+        }
+
+        SerialPort comPort = SerialPort.getCommPorts()[0];
+        comPort.openPort();
+
+    }
 }
