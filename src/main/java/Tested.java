fa04f1c70037fb13bf39ebaff32c1329c7399608
Alexander Shvets <box0259@gmail.com>
2022-01-16 22:13:42 +0400

    New code

--- src/main/java/Tested.java
+++ src/main/java/Tested.java
@@ -1,38 +1,46 @@
 
 import com.fazecast.jSerialComm.SerialPort;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
+import java.io.*;
 import java.util.*;
 
 
 /*
+* Класс описывает базовую работу и удобный протокол переачи массивов по последовательному порту
+* Массив является типом short (особенность совместимости с системами где int 2 байта и хранит числа от -32 768 до 32 767)
+* Имеется контроль crc, содержится в последней ячейке предоваймого массива (max value 32767)
+* Максимальное передоваймое значение массива является динамическим и рассчитывается по формуле maxValue = 32767 / lenght array (getMaxValue)
+*
+*
+* Доступные методы:
 * getArrayAllSerialPortsName() - возвращает массив String с именами всех доступных портов
 * getBaudRateList() - возвращает массив Int всех доступных бит рейтов
 *
 *
 *
 *
-*
+* ! необходимо реализовать проверку на наличие битрейта в битрейд листе
  */
 
 public class Tested {
     private static final int[] BAUD_RATE_LIST = {9600, 19200, 38400, 57600, 74880, 115200};
+    private final int maxValueArray;
+    private final short[] resultArray;
     private final SerialPort serialPort;
     private String openPortName;
     private int openPortBaudRate;
 
-    private Tested(String openPortName, int baudRate) {
+    private Tested(String openPortName, int baudRate, short[] resultArray) {
         this.openPortName = openPortName;
         serialPort = SerialPort.getCommPort(openPortName);
         serialPort.openPort();
         serialPort.setBaudRate(baudRate);
         openPortBaudRate = baudRate;
+        this.resultArray = resultArray;
+        maxValueArray = 0;
+        System.out.println("Open serial port: " +  openPortName);
     }
 
-    public static Tested getConnectSerialPort(String portName, int baudRate){
+    public static Tested getConnectSerialPort(String portName, int baudRate, short[] resultArray){
         if(portName == null){
             return null;
         }
@@ -41,7 +49,7 @@ public class Tested {
 
         String[] serialPortList = getArrayAllSerialPortsName();
         if(Arrays.binarySearch(serialPortList, portName) >= 0){
-            return new Tested(portName, baudRate);
+            return new Tested(portName, baudRate, resultArray);
         }
 
         return null;
@@ -64,6 +72,8 @@ public class Tested {
         openPortBaudRate = 0;
     }
 
+    public final short[] getResultArray(){return resultArray;}
+
     public final boolean isOpened(){return serialPort.isOpen();}
 
     public final String getOpenPortName() {return openPortName;}
@@ -81,10 +91,47 @@ public class Tested {
         return null;
     }
 
+    public final void test() {
+        try {
+            while (true) {
+                Thread.sleep(20);
+                while (serialPort.bytesAvailable()  > 0) {
+                    byte[] readBuffer = new byte[serialPort.bytesAvailable()];
+                    int numRead = serialPort.readBytes(readBuffer, readBuffer.length);
+                    parseFrame(readBuffer, numRead);
+
+                    /*
+                    char[] resultCharArray = new char[numRead];
+                    for(int i = 0; i < resultCharArray.length; i++){
+                        resultCharArray[i] = (char) readBuffer[i];
+                    }
+
+                    System.out.print(String.valueOf(resultCharArray));
+                    //System.out.println("Read " + numRead + " bytes.");
+                    //System.out.println(Arrays.toString(readBuffer));
+                    */
+
+                }
+            }
+        } catch (Exception e) { e.printStackTrace(); }
+        close();
+    }
+
+
+    private int[] parseFrame(byte[] frame, int quantityOfRealByte){
+        byte startSymbol = (byte) '$';
+        byte separatorSymbol = (byte) ' ';
+        byte finishSymbol = (byte) ';';
+
+
+        return new int[0];
+    }
 
 
 
-    public static void main(String[] args) {
+
+
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
 
         String[] serialPortList = getArrayAllSerialPortsName();
@@ -94,7 +141,39 @@ public class Tested {
 
        String scannerResult = scanner.nextLine().toUpperCase();
 
-       Tested tested = new Tested(scannerResult, getBaudRateList()[0]);
+         Tested tested = new Tested(scannerResult, getBaudRateList()[0]);
+
+       tested.test();
+
+
+      InputStream in = tested.getInStream();
+
+      // String result = IOUtils.toString(in, StandardCharsets.UTF_8);
+
+
+        /*
+
+        try {
+            ByteArrayOutputStream result = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = in.read(buffer)) != -1) {
+                result.write(buffer, 0, length);
+            }
+            System.out.println(result.toString("UTF-8"));
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            tested.close();
+        }
+
+         */
+
+
+
+
+
+
 
        // reader = new BufferedReader(new InputStreamReader(tested), StandardCharsets.UTF_8);
 
